74e739c31cbbc23542d1199ea9b7e8b070a089fc
Pandrapragada Himabindhu <Pandrapragada.Himabindhu@ibm.com>
2022-03-07 08:17:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deconstructor
+{
+
+    internal class Program
+    {
+        public static void main()
+        {
+            Console.ReadLine();
+        }
+        class home
+        {
+            public home()
+            {
+                Console.WriteLine("home class created");
+            }
+            ~home()
+            {
+                Console.WriteLine("home class destroved");
+            }
+        }
+    }
+}
